dd6b0f24806434e8dfa20fbd0f3819723b937fc5
ChipCheap <chip_44@hotmail.de>
2016-12-21 18:49:29 +0100

    Add files via upload

--- Sudoku.java
+++ Sudoku.java
@@ -1,10 +1,11 @@
 import java.util.Random;
 public class Sudoku {
 	private int[][] sudoku;
-	private Random r=new Random();
-	private boolean permutated=false;
+	private Random r=new Random();			//object of class Random to use methods for randomizing 
+	private boolean permutated=false;		//boolean for random permutations (see further below at randomPermutation( (n) )
 	public static void main(String[] args)
 	{
+		//test
 		Sudoku s=new Sudoku(10);
 //		s.permutateBand(2,1);
 //		s.permutateRows();
@@ -15,8 +16,6 @@ public class Sudoku {
 //		s.randomRow();
 //		s.hide(50);
 		System.out.println(s.toString());
-			
-		
 	}
 	/*
 	 * 
@@ -35,6 +34,10 @@ public class Sudoku {
 
 	 */
 //g)
+	/** constructor that generates new Sudoku with n permutations
+	 *	h) assigns new values to old ones, see other constructor
+	 * @param n amount of permutations to be done
+	 */
 	public Sudoku(int n)
 	{
 		int[] randomizer=randomRow();
@@ -62,7 +65,8 @@ public class Sudoku {
 	}
 //a)
 	/**
-	 * creates a predefined filled Sudoku
+	 * creates a predefined filled Sudoku (without //h))
+	 * with h), all the same old values get replaced with new values e.g. 1 becomes a 7 in the entire sudoku
 	 */
 	public Sudoku()
 	{
@@ -106,16 +110,20 @@ public class Sudoku {
 			}
 			if((i+1)%3==0)S+="\n-------------------------";
 		}
-			
 		return S;
 	}
-//c)	
+//c)
+	/**
+	 * swaps columns a and b under certain conditions
+	 * @param a col(umn) 1
+	 * @param b	col 2
+	 */
 	public void permutateColumns(int a, int b)
 	{
-		int temp;
-		if(a<0||b<0||a>8||b>8||a==b)return;
-		if(a>=0&&a<3&&b>=3)return;
-		if(a>=3&&a<6&&(b>=6||b<=4))return;
+		int temp;		//temporary save for a value
+		if(a<0||b<0||a>8||b>8||a==b)return;		//check if a, b inside 9x9 sudoku
+		if(a>=0&&a<3&&b>=3)return;				//check if a, b are in one block
+		if(a>=3&&a<6&&(b>=6||b<=2))return;
 		if(a>=6&&b<6)return;
 		for(int i=0;i<9;i++)
 		{
@@ -125,12 +133,17 @@ public class Sudoku {
 		}
 		permutated=true;
 	}
+	/**
+	 * swaps rows a and b
+	 * @param a row 1
+	 * @param b row 2
+	 */
 	public void permutateRows(int a, int b)
 	{
 		int temp;
-		if(a<0||b<0||a>8||b>8||a==b)return;		//check if a, b inside 9x9 sudoku
-		if(a>=0&&a<3&&b>=3)return;			//check if a, b are in one block
-		if(a>=3&&a<6&&(b>=6||b<=4))return;
+		if(a<0||b<0||a>8||b>8||a==b)return;		//checks for valid operations 
+		if(a>=0&&a<3&&b>=3)return;			
+		if(a>=3&&a<6&&(b>=6||b<=2))return;
 		if(a>=6&&b<6)return;
 		for(int i=0;i<9;i++)
 		{
@@ -141,10 +154,15 @@ public class Sudoku {
 		permutated=true;
 	}
 //d)	
+	/**
+	 * swaps triplets of columns (stacks) a and b 
+	 * @param a stack 1
+	 * @param b stack 2
+	 */
 	public void permutateStack(int a, int b)
 	{
 		int temp;
-		if(a<0||b<0||a>2||b>2||a==b)return;
+		if(a<0||b<0||a>2||b>2||a==b)return;		//i think you should have gotten the gist by now ;^)
 		for(int c=0;c<3;c++)
 		{
 			for(int i=0;i<9;i++)
@@ -156,10 +174,15 @@ public class Sudoku {
 		}
 		permutated=true;
 	}
+	/**
+	 * swaps triplets of rows a and b
+	 * @param a band 1
+	 * @param b band 2
+	 */
 	public void permutateBand(int a, int b)
 	{
 		int temp;
-		if(a<0||b<0||a>2||b>2||a==b)return;
+		if(a<0||b<0||a>2||b>2||a==b)return;		//Rockband a must not equal rockband b to not get sued and returned to jail
 		for(int c=0;c<3;c++)
 		{
 			for(int i=0;i<9;i++)
@@ -172,48 +195,53 @@ public class Sudoku {
 		permutated=true;
 	}
 //e)
+	/**
+	 * will definitely permutate 2 rows (by swapping them) once
+	 */
 	public void permutateRows()
 	{
 		int a=r.nextInt(9);
 		int b=r.nextInt(9);
-		while(!permutated)
+		while(!permutated)			//unless the permutating operation has been applied once, it shall be retried with another ranodm number for b
 		{
-			System.out.println("Rows err");
-			System.out.println("a: "+a);
-			System.out.println("b: "+b);
 			permutateRows(a,b);
 			b=r.nextInt(9);
 		}
 		permutated=false;
-		
 	}
+	/**
+	 * random columnswap 
+	 */
 	public void permutateColumns()
 	{
 		int a=r.nextInt(9);
 		int b=r.nextInt(9);
 		while(!permutated)
 		{
-			System.out.println("Coll err");
 			permutateColumns(a,b);
 			b=r.nextInt(9);
 		}
 		permutated=false;
 	}
+	/**
+	 * stacks
+	 */
 	public void permutateStack()
 	{
 		int a=r.nextInt(3);
 		int b=r.nextInt(3);
 		while(!permutated)
 		{
-			System.out.println("Stack err");
 			permutateStack(a,b);
 			b=r.nextInt(3);
 		}
 		permutated=false;
 	}
+	/**
+	 * Trolling Stones
+	 */
 	public void permutateBand()
 	{
-		System.out.println("Band err");
 		int a=r.nextInt(3);
 		int b=r.nextInt(3);
 		while(!permutated)
@@ -224,6 +252,9 @@ public class Sudoku {
 		permutated=false;
 	}
 //f)
+	/**
+	 * mirrors the puzzle on the topleft to botright diagonal
+	 */
 	public void transpose()
 	{
 		int temp=0;
@@ -238,9 +269,12 @@ public class Sudoku {
 		}
 	}
 //g)
+	/**
+	 * will do one of the 4 base mutations
+	 * and afterwards there is a chance that the puzzle will get transposed as well(transposing twice is like not transposing at all, wastes time)
+	 */
 	public void randomPermutation()
 	{
-		System.out.println("works");
 		int i=r.nextInt(4);
 		switch(i)
 		{
@@ -255,11 +289,14 @@ public class Sudoku {
 	{
 		for(int i=0;i<n;i++)
 		{
-			System.out.println("test");
 			randomPermutation();
 		}
 	}
 //h)
+	/**
+	 * randomizes a new order for the numbers 1-9 to create more puzzles in the constructor
+	 * @return int array with the randomized order
+	 */
 	public int[] randomRow()
 	{
 		boolean reset=false;
@@ -286,6 +323,10 @@ public class Sudoku {
 		return randomized;
 	}
 //i)
+	/**
+	 * hides up to n spaces in the sudoku, hiding the same square twice has absolutely no effect
+	 * @param n amount of tries to hide numbers
+	 */
 	public void hide(int n)
 	{
 		int a=r.nextInt(9);
@@ -298,4 +339,3 @@ public class Sudoku {
 		}
 	}
 }
-//t-d: permutations are erroring at times, fix, probably if(..) return; wrong somewhere
